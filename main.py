be22a953da0640dbdb6d42caa0247b61335a0db5
SlientShhh <153305811+SilentDev-09@users.noreply.github.com>
2024-02-04 13:32:34 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+client = commands.Bot(command_prefix="e!", intents=discord.Intents().all())
+
+sync = await client.tree.sync()
+@client.event
+async def on_ready():
+  
+  print("Bot online")
+  print("Comandi slash:" + sync)
+@client.tree.command(description="Test")
+async def test(i: discord.Interaction):
+  try:
+    await i.response.send_message("Sono vivo! I miei comandi slash sono: " + sync)
+  except Exception as e:
+    await i.response.send_message("Errore")
+    raise e
+client.run("MTIwMzY3MDY1MTQ1ODU1NTkyNA.GrqS3H.dl_BPk-L1bL9Twe4sjVp17nHC2JhhIQv9SHT-I")
